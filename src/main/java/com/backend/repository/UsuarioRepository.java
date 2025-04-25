8ce555f1549e1f9f135f5676a263021e7cd78cd7
Marco Orellana <mafeorellana09@gmail.com>
2025-04-25 13:57:57 -0400

    ambas tablas listas, probado por swagger y postman

--- src/main/java/com/backend/repository/UsuarioRepository.java
+++ src/main/java/com/backend/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.backend.repository;
 
 import com.backend.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
