e3c400cdcc0ce39fb4068b1f5a9295631694cb0d
Marco Orellana <mafeorellana09@gmail.com>
2025-04-28 11:59:46 -0400

    para guardar ambos unidos

--- src/main/java/com/backend/config/SwaggerConfig.java
+++ src/main/java/com/backend/config/SwaggerConfig.java
@@ -13,6 +13,15 @@ public class SwaggerConfig {
         return new OpenAPI().info(new Info()
                 .title("Microservicio de Usuarios API-REST")
                 .version("1.0")
-                .description("Documentación generada con Swagger"));
+                .description("Bienvenido a la documentación de la API Usuarios.\n" +
+                        "\n" +
+                        "            - Para listar usuarios, utiliza el endpoint `GET /usuarios`.\n" +
+                        "            - Para crear un usuario, usa `POST /usuarios`.\n" +
+                        "            - Para actualizar, usa `PUT /usuarios/{id}`.\n" +
+                        "            - Para eliminar, usa `DELETE /usuarios/{id}`.\n" +
+                        "\n" +
+                        "            Cada endpoint muestra los parámetros requeridos y los posibles códigos de respuesta.\n" +
+                        "\n" +
+                        "            ¡Explora la documentación usando el menú desplegable de la izquierda o el buscador!"));
     }
 }
