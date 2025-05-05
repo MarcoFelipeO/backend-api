54d2d9bce05d1fa45c121aa59c9e44a82b05c6d2
Marco Orellana <mafeorellana09@gmail.com>
2025-05-05 13:46:18 -0400

    cambios

--- src/main/java/com/backend/dto/TelefonoDTO.java
+++ src/main/java/com/backend/dto/TelefonoDTO.java
@@ -6,5 +6,37 @@ public class TelefonoDTO {
     private String numero;
     private String tipo;
 
+    public TelefonoDTO() {
+    }
+
+    public TelefonoDTO(Long telefono_id, String numero, String tipo) {
+        this.telefono_id = telefono_id;
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+
+    public Long getTelefono_id() {
+        return telefono_id;
+    }
+
+    public void setTelefono_id(Long telefono_id) {
+        this.telefono_id = telefono_id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
 }
--- src/main/java/com/backend/dto/UsuarioDTO.java
+++ src/main/java/com/backend/dto/UsuarioDTO.java
@@ -14,6 +14,56 @@ public class UsuarioDTO {
     private List<TelefonoDTO> telefonos;
 
 
+    //constructor vacio
+    public UsuarioDTO() {
+    }
 
+    public UsuarioDTO(Long usuario_id, String nombres, String apellidos, String correo, List<TelefonoDTO> telefonos) {
+        this.usuario_id = usuario_id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.telefonos = telefonos;
+    }
+
+    public Long getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(Long usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public List<TelefonoDTO> getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(List<TelefonoDTO> telefonos) {
+        this.telefonos = telefonos;
+    }
 }
 
